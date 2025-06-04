ae201db85d3006001249b7d83dcd12e34d64248e
michaelouma <moketchus12@gmail.com>
2025-06-04 15:19:35 +0300

    commit changes

--- neurons.py
+++ neurons.py
@@ -23,3 +23,17 @@ import numpy as np
 
 output1 = np.dot(weights, inputs) + biases
 print(output1)
+
+## What about multiplying matrix and matrix
+
+inputs = [[1,2,3,2.5],
+          [2.0,5.0,-1.0, 2.0],
+          [-1.5,2.7,3.3,-0.8]]
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2,3,0.5]
+
+output2 =np.dot(inputs, np.array(weights).T) + biases
+print(output2)
